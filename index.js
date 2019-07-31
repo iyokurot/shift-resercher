6a52456de8a99108a8aa7a6ee9894d444281073b
iyokurot <01kayochin17neko@gmail.com>
2019-07-31 17:43:32 +0900

    login

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const line_login = require("line-login");
+require("dotenv").config();
 
 const login = new line_login({
     channel_id: process.env.LINE_LOGIN_CHANNEL_ID,
