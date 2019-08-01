c869dfbdd33298022b22b50c0ff3fd4942f25ba4
iyokurot <01kayochin17neko@gmail.com>
2019-08-01 22:41:33 +0900

    login

--- index.js
+++ index.js
@@ -160,7 +160,7 @@ app.get('/regist', async (req, res) => {
             req.session.username = results.name;
             req.session.worktime = results.worktime;
             req.session.administer = results.administer;
-            res.send(results[0].name);
+            res.send(results + "::" + results.name);
             //res.redirect('/Home');
         }
         client.release();
