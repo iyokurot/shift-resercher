2314391e6fb4e772d655a1be5252e0d3f046f0b1
iyokurot <01kayochin17neko@gmail.com>
2019-08-12 21:46:04 +0900

    react

---
+++ client/build/precache-manifest.81407e5fbe3245cf47617e8c8e389cfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93c704c9abf09d70c9108e28faf2f0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7748afb3ec3e411e012d",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6ca35b2c6f6cbd7dd3e7",
+    "url": "/static/js/2.4dd4f011.chunk.js"
+  },
+  {
+    "revision": "7748afb3ec3e411e012d",
+    "url": "/static/js/main.37a824a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
