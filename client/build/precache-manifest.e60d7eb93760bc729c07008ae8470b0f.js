2f158ab6ab10fa3155c15511d13a7fe46edf064d
iyokurot <01kayochin17neko@gmail.com>
2019-08-16 06:15:39 +0900

    react-calendar

---
+++ client/build/precache-manifest.e60d7eb93760bc729c07008ae8470b0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "718a30e13f4cb574718842aef0d74196",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58511ee51a3f2831d67d",
+    "url": "/static/css/2.c51b6406.chunk.css"
+  },
+  {
+    "revision": "d6021dd835923b5b6049",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "58511ee51a3f2831d67d",
+    "url": "/static/js/2.aefaa420.chunk.js"
+  },
+  {
+    "revision": "d6021dd835923b5b6049",
+    "url": "/static/js/main.c6c6faa7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9040ffb32861bfd34f4c5a2eae988c45.js"
+  "/precache-manifest.e60d7eb93760bc729c07008ae8470b0f.js"
 );
 
 self.addEventListener('message', (event) => {
