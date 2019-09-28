5df66e13751f8c358ae6ffd0208ab03fca016724
itoyu <itoyu@CPSLab-no-MacBook-Air.local>
2019-09-28 12:42:42 +0900

    2019/0928

--- client/src/Home.js
+++ client/src/Home.js
@@ -255,7 +255,7 @@ class Home extends Component {
       </div>
     )
   }
-  //yyMMdd
+  //yyMMdd日付
   getintdate(year, month, day) {
     return year + ('0' + (month + 1)).slice(-2) + ('0' + day).slice(-2)
   }
