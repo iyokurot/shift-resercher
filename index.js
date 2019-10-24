9ca59028ba57b95ccb68b444ff3bfff6d5ceb4c6
iyokurot <01kayochin17neko@gmail.com>
2019-10-24 19:13:43 +0900

    webhook

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use((req, res, next) => {
 })
 app.post('/line', function(req, res) {
   console.log(req)
-  res.status(200)
+  res.status(200).end()
 })
 
 app.get('/userdata', function(req, res) {
