a7985d94a462d1af57e9bff4e2622c8ff9951ca1
iyokurot <01kayochin17neko@gmail.com>
2020-03-20 06:15:30 +0900

    mail setting update

--- mailsender.js
+++ mailsender.js
@@ -1,9 +1,10 @@
+//nodemailerテスト用script
+
 var receiverEmailAddress = '03yuki18.kp@gmail.com'
-var senderEmailAddress = '03yuki18.kp@gmail.com'
-var senderEmailPassword = 'kayopile'
+const senderEmailAddress = 'shiftresercher@gmail.com'
 
 var nodemailer = require('nodemailer')
-var transporter = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
   secure: true, // SSL
@@ -11,17 +12,21 @@ var transporter = nodemailer.createTransport({
     type: 'OAuth2',
     user: senderEmailAddress,
     clientId:
-      '662087203204-o4s6jqkd5rml89a4jqtb736m47ncg94t.apps.googleusercontent.com',
-    clientSecret: 'Fg6HAPWL25mOynnOI1UpHx_K',
-    accessToken:
-      'ya29.Il-7BzNTwIFf9DR5bxj9yngguRlIv62bhsEjlmBp_nyWxL35_JCt2gG7TskJWRLv1dHHl2u--WDGPV4XCuu2uFNESYn3nGfYIaUzbFEPgPxLro_bSiPSWk3zkD-SZcPq2A',
+      '467833761077-l0epkhdmmvlq6e1pdgq0g9flupnp7q9v.apps.googleusercontent.com',
+    clientSecret: 'pwriA5eQCQEYT7TPwJcJNXVH',
+    refreshToken:
+      '1//04Z5o55bN7Fp1CgYIARAAGAQSNwF-L9IrbKOq9F3LWlomGo5PdZVqI-InuRszHzNlTYe-hhS0p54eRtVFWXs6fVyuXPDXmawigAM',
   },
 })
 var mailOptions1 = {
   from: senderEmailAddress,
   to: receiverEmailAddress,
-  subject: 'mailer test',
-  text: 'test message2',
+  subject: 'シフト締め切り通知',
+  text: 'Hello to myself!',
+  html:
+    '<h2>本日シフト締切日です！</h2>' +
+    '<p>https://shift-resercher.herokuapp.com/</p>' +
+    '<button>onclick</button>',
 }
 transporter.sendMail(mailOptions1, function(error, info) {
   if (error) {
